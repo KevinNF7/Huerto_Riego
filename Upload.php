2605d97100e29011aaf8592e2e35cb108f382a26
KevinNF7 <85267283+KevinNF7@users.noreply.github.com>
2024-11-04 22:56:23 -0500

    Update Upload.php

--- Upload.php
+++ Upload.php
@@ -4,17 +4,15 @@ date_default_timezone_set('America/Lima');
 $servername = "127.0.0.1";
 $username = "Usuario";
 $password = "Contraseña";
-$dbname = "Nombre_base_de_datos";
-echo "Se ejecuto php";
+$dbname = "u975775819_HuertoTomates";
+
+
 $conn = new mysqli($servername,$username,$password,$dbname);
 
 if($conn -> connect_error){
 	die("Conexion fallida: ". $conn->connect_error);
-	echo "Fallo en la coneccion";
-	
-}else{
-    echo "Coneccion exitosa";
 }
+
 $fecha = date("Ymd");
 
 // Nombre de la tabla basada en la fecha
@@ -32,72 +30,57 @@ $sql_tabla = "CREATE TABLE IF NOT EXISTS $nombre_tabla (
     Humedad7 FLOAT,
     PotenciaINA FLOAT,
     CorrienteINA FLOAT,
-    CorrienteACS FLOAT,
     NivelDeAgua FLOAT,
     Fertilizante FLOAT,
     Temperatura FLOAT
 )";
 
 if ($conn->query($sql_tabla) === TRUE) {
-    echo "Tabla $nombre_tabla creada o ya existente.";
+    //Tabla creada
 } else {
-    echo "Error al crear la tabla: " . $conn->error;
+    //echo "Error al crear la tabla: " . $conn->error;
 }
 
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     echo "Dentro del metodo POST";
-	//$data = $_POST["data"];
-    //echo $data;
-	//$data_array = explode("&",$data);
-	$humedad1 = $_POST['humidity1'];
-	$humedad2 = $_POST['humidity2'];
-	$humedad3 = $_POST['humidity3'];
-	$humedad4 = $_POST['humidity4'];
-	$humedad5 = $_POST['humidity5'];
-	$humedad6 = $_POST['humidity6'];
-	$humedad7 = $_POST['humidity7'];
-	$potencia_ina = $_POST['power_ina'];
-	$corriente_ina = $_POST['current_ina'];
-	$corriente_acs = $_POST['current_acs'];
-	$agua = $_POST['water_level'];
-	$fertilizante = $_POST['fertilizer'];
-	$temperatura = $_POST['temperature'];
-	//echo "Humedad= "  . $humedad;
-//	list($humedad,$potencia,$agua,$fertilizante,$tempertura) = explode("&",$data);
-    //echo $data;
+
+	$humedad1 = $_POST['humedad1'];
+	$humedad2 = $_POST['humedad2'];
+	$humedad3 = $_POST['humedad3'];
+	$humedad4 = $_POST['humedad4'];
+	$humedad5 = $_POST['humedad5'];
+	$humedad6 = $_POST['humedad6'];
+	$humedad7 = $_POST['humedad7'];
+	$potenciaINA = $_POST['potenciaINA'];
+	$corrienteINA = $_POST['corrienteINA'];
+	$nivelDeAgua = $_POST['nivelDeAgua'];
+	$fertilizante = $_POST['fertilizante'];
+	$temperatura = $_POST['temperatura'];
+
     $sql = "";
     $sql2 = "";
+
     if($humedad1 == 0 && $humedad2 ==0 && $corriente_ina != 0 && $potencia_ina!=0){
-        $sql = "INSERT INTO Datos_electricidad (PotenciaINA,CorrienteINA,CorrienteACS) VALUES ('$potencia_ina','$corriente_ina','$corriente_acs')";
+        $sql = "INSERT INTO Datos_electricidad (PotenciaINA,CorrienteINA) VALUES ('$potenciaINA','$corrienteINA')";
     }elseif($humedad1 == 0 && $humedad2 ==0 && $corriente_ina == 0 && $potencia_ina==0){
         $sql = "";
     }
     else{
-	    $sql = "INSERT INTO $nombre_tabla (Humedad1,Humedad2,Humedad3,Humedad4,Humedad5,Humedad6,Humedad7,PotenciaINA,CorrienteINA,CorrienteACS,NivelDeAgua,Fertilizante,Temperatura) VALUES ('$humedad1','$humedad2','$humedad3','$humedad4','$humedad5','$humedad6','$humedad7','$potencia_ina','$corriente_ina','$corriente_acs','$agua','$fertilizante','$temperatura')";
-	    $sql2 = "INSERT INTO Datos_electricidad (PotenciaINA,CorrienteINA,CorrienteACS) VALUES ($potencia_ina','$corriente_ina','$corriente_acs')";
+	    $sql = "INSERT INTO $nombre_tabla (Humedad1,Humedad2,Humedad3,Humedad4,Humedad5,Humedad6,Humedad7,PotenciaINA,CorrienteINA,NivelDeAgua,Fertilizante,Temperatura) VALUES ('$humedad1','$humedad2','$humedad3','$humedad4','$humedad5','$humedad6','$humedad7','$potenciaINA','$corrienteINA','$nivelDeAgua','$fertilizante','$temperatura')";
+	    $sql2 = "INSERT INTO Datos_electricidad (PotenciaINA,CorrienteINA) VALUES ($potencia_ina','$corriente_ina')";
     }
-//	echo $humedad;
-////ESTE METODO TAMBIEN FUNCIONA
-//	if($conn->query($sql) === TRUE){
-	    //echo "Nuevo registro creado";
 
-//	}else{
-	    //echo "Error";
-//	}
-    if (mysqli_query($conn, $sql)) {
-      echo "New record created successfully";
+    if ($conn->query($sql) === TRUE) {
+        //Se creo una nueva fila
     } else {
-//      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        //echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
-    if(mysqli_query($conn, $sql2)){
-        echo "New record";
-    } else{
-        
-    }
+    if($conn->query($sql2) === TRUE){
+        //Se creo una nueva fila
+    } 
     
-}else{
-	echo "No se han envieado nada";
 }
+
 $conn ->close();
 ?>
